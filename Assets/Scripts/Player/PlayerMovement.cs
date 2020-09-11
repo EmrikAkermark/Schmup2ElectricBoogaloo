7ab49f08b49fd26d94844e82e0c87219f7fdf58d
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-11 17:51:46 +0200

    Added first player movement, it handles funny

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public KeyCode Up, Down, Left, Right;
+
+    public float speed;
+    private Vector2 direction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        KeyboardMove();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(Right))
+        {
+            direction.x += 1;
+        }
+        if (Input.GetKey(Left))
+        {
+            direction.x -= 1;
+        }
+        if (Input.GetKey(Up))
+        {
+            direction.y += 1;
+        }
+        if (Input.GetKey(Down))
+        {
+            direction.y -= 1;
+        }
+    }
+
+    private void KeyboardMove()
+    {
+        Vector2 currentPosition = transform.position;
+        direction *= Time.fixedDeltaTime * speed;
+        rb.MovePosition(currentPosition + direction);
+    }
+}
