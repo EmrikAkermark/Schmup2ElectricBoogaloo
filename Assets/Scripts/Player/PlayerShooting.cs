f1f76b8d859a3a9e3799f4af1fadc97e1d76fe1d
Emrik Åkermark <headcrab.farmer@gmail.com>
2020-09-16 14:08:13 +0200

    Forgot new scripts

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public KeyCode Shoot;
+
+    private PlayerWeapons pWeapons;
+
+	private void Start()
+	{
+		pWeapons = GetComponent<PlayerWeapons>();
+	}
+
+	void Update()
+    {
+        if(Input.GetKey(Shoot))
+		{
+            pWeapons.Fire();
+		}
+    }
+}
